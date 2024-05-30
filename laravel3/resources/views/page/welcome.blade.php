2436fe8a4e573c868e30eb03211234bc4641f380
rama putra <ramaputrafriandy@gmail.com>
2024-05-30 16:05:44 +0700

    laravel migration

---
+++ laravel3/resources/views/page/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+@section('title')
+    Halaman Home
+@endsection
+@section('content')
+<h1>SELAMAT DATANG {{$fname}} {{$lname}}</h1>
+<h2>Terima Kasih telah bergabung di Sanberbook. Social Media kita bersama!</h2>
+@endsection
+        
